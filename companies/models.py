801a8889b451d8f5a62d019687e55ee1fe4a285f
joaomacedocb <joaomacedocb@gmail.com>
2024-12-21 00:55:33 -0300

    Correct circular import in companies.models

--- companies/models.py
+++ companies/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from accounts.models import User
-
 class Company(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
