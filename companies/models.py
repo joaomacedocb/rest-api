a8e66cec5d653311c3494adc0cca8418a0790e82
joaomacedocb <joaomacedocb@gmail.com>
2024-12-22 16:45:53 -0300

    Add class Employee to companies models

--- companies/models.py
+++ companies/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    
+class Employee(models.Model):
+    user = models.ForeignKey("accounts.user", on_delete=models.CASCADE)
+    company = models.ForeignKey("companies.company", on_delete=models.CASCADE)
+    
